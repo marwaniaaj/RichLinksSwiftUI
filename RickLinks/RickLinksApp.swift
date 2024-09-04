2b19388eb303ba6f1cdeb328ae29ada27fe63ff5
Marwa <marwa.niaaj@gmail.com>
2024-09-04 17:59:09 +0400

    Initial Commit

---
+++ RickLinks/RickLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickLinksApp.swift
+//  RickLinks
+//
+//  Created by Marwa Abou Niaaj on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
